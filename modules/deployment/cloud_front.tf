8034de19ce0567de0ccc5a8c0b8f00b1e0374de3
Andy Pliszka <andy@isp44.com>
2024-11-21 11:31:14 -0500

    refactor: simplifies setup between CloudFront and hosting S3 bucket
    * locks down the S3 bucket permissions
    * uses dedicated policy for CF

--- modules/deployment/cloud_front.tf
+++ modules/deployment/cloud_front.tf
@@ -1,14 +1,19 @@
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_origin_access_control" "access" {
+  name                              = "CloudFront S3"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 resource "aws_cloudfront_distribution" "distribution" {
   origin {
-    domain_name = aws_s3_bucket_website_configuration.configuration.website_endpoint
-    origin_id   = "S3-www.${var.domain}"
-
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "https-only"
-      origin_ssl_protocols   = ["TLSv1.2", "SSLv3"]
-    }
+    domain_name              = aws_s3_bucket.html.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.access.id
+    origin_id                = local.s3_origin_id
   }
 
   enabled             = true
@@ -20,17 +25,10 @@ resource "aws_cloudfront_distribution" "distribution" {
     "${var.domain}"
   ]
 
-  custom_error_response {
-    error_caching_min_ttl = 0
-    error_code            = 404
-    response_code         = 200
-    response_page_path    = "/404.html"
-  }
-
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-www.${var.domain}"
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -40,24 +38,69 @@ resource "aws_cloudfront_distribution" "distribution" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 31536000
-    default_ttl            = 31536000
-    max_ttl                = 31536000
-    compress               = true
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+   # Cache behavior with precedence 0
+#  ordered_cache_behavior {
+#    path_pattern     = "/content/immutable/*"
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#    target_origin_id = local.s3_origin_id
+#
+#    forwarded_values {
+#      query_string = false
+#      headers      = ["Origin"]
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    min_ttl                = 0
+#    default_ttl            = 86400
+#    max_ttl                = 31536000
+#    compress               = true
+#    viewer_protocol_policy = "redirect-to-https"
+#  }
+
+  # Cache behavior with precedence 1
+#  ordered_cache_behavior {
+#    path_pattern     = "/content/*"
+#    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = local.s3_origin_id
+#
+#    forwarded_values {
+#      query_string = false
+#
+#      cookies {
+#        forward = "none"
+#      }
+#    }
+#
+#    min_ttl                = 0
+#    default_ttl            = 3600
+#    max_ttl                = 86400
+#    compress               = true
+#    viewer_protocol_policy = "redirect-to-https"
+#  }
+#
+#  price_class = "PriceClass_200"
+#
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "whitelist"
+#      locations        = ["US", "CA", "GB", "DE"]
+#    }
+#  }
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.cert.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
-
-  depends_on = [aws_s3_bucket_policy.policy, aws_acm_certificate.cert, aws_acm_certificate_validation.cert-validation]
 }
--- modules/deployment/s3.tf
+++ modules/deployment/s3.tf
@@ -2,68 +2,41 @@ resource "aws_s3_bucket" "html" {
   bucket = "${var.domain}"
 }
 
-resource "aws_s3_bucket_ownership_controls" "ownershiop" {
+data "aws_iam_policy_document" "s3_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.domain}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.distribution.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
+  bucket = aws_s3_bucket.html.id
+  policy = data.aws_iam_policy_document.s3_bucket_policy.json
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.html.id
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "access" {
-  bucket = aws_s3_bucket.html.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket_acl" "acl" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.ownershiop,
-    aws_s3_bucket_public_access_block.access,
+    aws_s3_bucket_ownership_controls.ownership
   ]
 
   bucket = aws_s3_bucket.html.id
   acl    = "private"
-}
-
-resource "aws_s3_bucket_cors_configuration" "cors" {
-  bucket = aws_s3_bucket.html.id
-
-  cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
-    allowed_origins = [
-      "https://www.${var.domain}",
-    ]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-
-  cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-  }
-}
-
-resource "aws_s3_bucket_website_configuration" "configuration" {
-  bucket = aws_s3_bucket.html.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.html.id
-  policy = templatefile("./modules/deployment/s3-policy.json", { bucket = "${var.domain}" })
-
-  depends_on = [aws_s3_bucket_ownership_controls.ownershiop, aws_s3_bucket.html, aws_s3_bucket_acl.acl]
 }
 
 resource "aws_s3_object" "index-html" {
